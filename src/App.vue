0982d0a83357825c2c935f9fe378d4dae42ec278
thissfredo <thissfredo@gmail.com>
2024-11-25 11:32:25 -0300

    Cria aplicação

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <TipCalculator />
+  </div>
+</template>
+
+<script>
+import TipCalculator from './components/TipCalculator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TipCalculator
+  }
+}
+</script>
+
+<style>
+</style>
